081f8f14c56725c49d15184380a9648c4c02e6a2
ZOI\graham.geary <gg@zoi.de>
2018-09-20 10:12:54 +0200

    "role_name" variable is now "role_names" and type "list"

--- variables.tf
+++ variables.tf
@@ -6,14 +6,15 @@ variable "aws_region" {
   description = "The reqion where the ressrouces should be create. Unimportant as IAM."
   default = "eu-central-1"
 }
-variable "role_name" {
-  description = "The name of the Role"
+variable "role_names" {
+  type = "list"
+  description = "The name(s) of the Role(s)"
   default = "Delegate-Saml-Admin"
 }
 variable "role_policies" {
+  type = "list"
   description = "The policies attached to the role"
   default = ["arn:aws:iam::aws:policy/AdministratorAccess"]
-  type = "list"
 }
 
 variable "provider_name" {
