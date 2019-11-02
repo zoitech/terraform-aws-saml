c5ad8acbddd63cc1a254d6f702eff37a47c54104
ZOI\graham.geary <gg@zoi.de>
2019-11-02 11:33:49 +0100

    #10 Make billing and logging role independent from one another #11 Enable all roles to take a custom policy

--- locals.tf
+++ locals.tf
@@ -1,35 +1,40 @@
 locals {
   roles         = var.roles == null ? local.default_roles : var.roles
-  default_roles = var.create_all_roles == true ? local.all_roles : local.standard_roles
+  default_roles = var.create_billing_role == true && var.create_logging_role == true ? local.all_roles : var.create_billing_role == true && var.create_logging_role == false ? concat(local.standard_roles, local.billing_role) : var.create_billing_role == false && var.create_logging_role == true ? concat(local.standard_roles, local.logging_role) : local.standard_roles
   standard_roles = [
     {
       role_name        = "SAML_FullAccess"
-      role_policy_arn  = "arn:aws:iam::aws:policy/AdministratorAccess"
+      role_policy_arn  = var.fullaccess_role_custom_policy_arn == null ? "arn:aws:iam::aws:policy/AdministratorAccess" : var.fullaccess_role_custom_policy_arn == null
       role_description = "ROLE_AWS_${data.aws_caller_identity.current.account_id}_FullAccess"
     },
     {
       role_name        = "SAML_ReadOnly"
-      role_policy_arn  = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+      role_policy_arn  = var.readonly_role_custom_policy_arn == null ? "arn:aws:iam::aws:policy/ReadOnlyAccess" : var.readonly_role_custom_policy_arn
       role_description = "ROLE_AWS_${data.aws_caller_identity.current.account_id}_ReadOnly"
     },
     {
       role_name        = "SAML_PowerUser"
-      role_policy_arn  = "arn:aws:iam::aws:policy/PowerUserAccess"
+      role_policy_arn  = var.poweruser_role_custom_policy_arn == null ? "arn:aws:iam::aws:policy/PowerUserAccess" : var.poweruser_role_custom_policy_arn
       role_description = "ROLE_AWS_${data.aws_caller_identity.current.account_id}_PowerUserAccess"
     },
   ]
 
-  all_roles = concat(local.standard_roles, [
+  billing_role = [
     {
       role_name        = "SAML_Billing"
-      role_policy_arn  = "arn:aws:iam::aws:policy/job-function/Billing"
+      role_policy_arn  = var.billing_role_custom_policy_arn == null ? "arn:aws:iam::aws:policy/job-function/Billing" : var.billing_role_custom_policy_arn
       role_description = "ROLE_AWS_${data.aws_caller_identity.current.account_id}_Billing"
     },
+  ]
+
+  logging_role = [
     {
       role_name        = "SAML_Logging"
-      role_policy_arn  = "arn:aws:iam::aws:policy/CloudWatchLogsReadOnlyAccess"
+      role_policy_arn  = var.logging_role_custom_policy_arn == null ? "arn:aws:iam::aws:policy/CloudWatchLogsReadOnlyAccess" : var.logging_role_custom_policy_arn
       role_description = "ROLE_AWS_${data.aws_caller_identity.current.account_id}_Logging - Read Only"
-    }
-  ])
+    },
+  ]
+
+  all_roles = concat(local.standard_roles, local.billing_role, local.logging_role)
 
 }
--- variables.tf
+++ variables.tf
@@ -3,9 +3,9 @@ variable "provider_name" {
   default     = "ADFS"
 }
 
-variable "create_all_roles" {
-  description = "Add billing and logging roles"
-  default     = false
+variable "provider_metadata_file" {
+  description = "The path to of the metadatafile"
+  default     = null
 }
 
 variable "roles" {
@@ -17,7 +17,40 @@ variable "roles" {
   }))
   default = null
 }
-variable "provider_metadata_file" {
-  description = "The path to of the metadatafile"
+
+variable "fullaccess_role_custom_policy_arn" {
+  description = "Custom policy ARN for the SAML_FullAccess role"
   default     = null
 }
+
+variable "poweruser_role_custom_policy_arn" {
+  description = "Custom policy ARN for the SAML_PowerUser role"
+  default     = null
+}
+
+variable "readonly_role_custom_policy_arn" {
+  description = "Custom policy ARN for the SAML_ReadOnly role"
+  default     = null
+}
+
+variable "create_billing_role" {
+  description = "Add billing role"
+  default     = false
+}
+
+variable "billing_role_custom_policy_arn" {
+  description = "Custom policy ARN for the SAML_Billing role"
+  default     = null
+}
+
+variable "create_logging_role" {
+  description = "Add logging role"
+  default     = false
+}
+
+variable "logging_role_custom_policy_arn" {
+  description = "Custom policy ARN for the SAML_Logging role"
+  default     = null
+}
+
+
