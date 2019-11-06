8c3f8da870e4c21d123bbaf35a5e37a8e5fdd7b2
ZOI\graham.geary <gg@zoi.de>
2019-11-06 08:39:29 +0100

    remove unused variables

--- variables.tf
+++ variables.tf
@@ -18,39 +18,4 @@ variable "roles" {
   default = null
 }
 
-variable "fullaccess_role_custom_policy_arn" {
-  description = "Custom policy ARN for the SAML_FullAccess role"
-  default     = null
-}
-
-variable "poweruser_role_custom_policy_arn" {
-  description = "Custom policy ARN for the SAML_PowerUser role"
-  default     = null
-}
-
-variable "readonly_role_custom_policy_arn" {
-  description = "Custom policy ARN for the SAML_ReadOnly role"
-  default     = null
-}
-
-variable "create_billing_role" {
-  description = "Add billing role"
-  default     = false
-}
-
-variable "billing_role_custom_policy_arn" {
-  description = "Custom policy ARN for the SAML_Billing role"
-  default     = null
-}
-
-variable "create_logging_role" {
-  description = "Add logging role"
-  default     = false
-}
-
-variable "logging_role_custom_policy_arn" {
-  description = "Custom policy ARN for the SAML_Logging role"
-  default     = null
-}
-
 
